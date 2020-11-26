6353ee7765b050059e418bd39748273957093da6
wotusay <wout.salembier@hotmail.com>
2020-11-26 15:52:59 +0100

    got the server up and running

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+// Load required modules
 require('dotenv').config();
 const isDevelopment = (process.env.NODE_ENV === 'development');
-const express = require('express');
-const app = express();
-const fs = require('fs');
+const fs = require("fs"); // http server core module
+
+const express = require("express"); // web framework external module
+
+// Set process name
+process.title = "networked-aframe-server";
 
 let options = {};
 if (isDevelopment) {
@@ -12,11 +16,74 @@ if (isDevelopment) {
   };
 }
 
-const server = require(isDevelopment ? 'https' : 'http').Server(options, app);
+// Get port or default to 8080
 const port = process.env.PORT || 443;
 
+// Setup and configure Express http server.
+const app = express();
 app.use(express.static('public'));
 
-server.listen(port, () => {
- console.log(`App listening on port ${port}!`);
+// Serve the example and build the bundle in development.
+
+
+// Start Express http server
+const webServer =  require(isDevelopment ? 'https' : 'http').Server(options, app);;
+const io = require("socket.io")(webServer);
+
+const rooms = {};
+
+io.on("connection", socket => {
+  console.log("user connected", socket.id);
+
+  let curRoom = null;
+
+  socket.on("joinRoom", data => {
+    const { room } = data;
+
+    if (!rooms[room]) {
+      rooms[room] = {
+        name: room,
+        occupants: {},
+      };
+    }
+
+    const joinedTime = Date.now();
+    rooms[room].occupants[socket.id] = joinedTime;
+    curRoom = room;
+
+    console.log(`${socket.id} joined room ${room}`);
+    socket.join(room);
+
+    socket.emit("connectSuccess", { joinedTime });
+    const occupants = rooms[room].occupants;
+    io.in(curRoom).emit("occupantsChanged", { occupants });
+  });
+
+  socket.on("send", data => {
+    io.to(data.to).emit("send", data);
+  });
+
+  socket.on("broadcast", data => {
+    socket.to(curRoom).broadcast.emit("broadcast", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log('disconnected: ', socket.id, curRoom);
+    if (rooms[curRoom]) {
+      console.log("user disconnected", socket.id);
+
+      delete rooms[curRoom].occupants[socket.id];
+      const occupants = rooms[curRoom].occupants;
+      socket.to(curRoom).broadcast.emit("occupantsChanged", { occupants });
+
+      if (occupants == {}) {
+        console.log("everybody left room");
+        delete rooms[curRoom];
+      }
+    }
+  });
+});
+
+webServer.listen(port, () => {
+  console.log("listening on https://localhost:" + port);
 });
