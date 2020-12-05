97886d07a269b7f1f1b39ae41fb48dc6874a5041
wotusay <wout.salembier@hotmail.com>
2020-12-05 20:00:33 +0100

    added elements

--- public/js/begin-screen.js
+++ public/js/begin-screen.js
@@ -2,9 +2,11 @@ const buttonScale = "0.7 0.7 0.7";
 const buttonUpScale = "0.9 0.9 0.9";
 const dur = '500';
 
+
+
 AFRAME.registerComponent('start-event', {
     init: function() {
-
+        const menu = document.querySelector('.menu');
         this.el.addEventListener('mouseleave', e => {
             this.el.setAttribute('animation',`property:scale; to:${buttonScale}; dur:${dur};`);
         })
@@ -15,12 +17,13 @@ AFRAME.registerComponent('start-event', {
         
         this.el.addEventListener('click', e =>  {
             this.el.setAttribute('visible', 'false');
+            menu.setAttribute('visible', 'true');
         })
     }
 });
 
 
-AFRAME.registerComponent('public-prak', {
+AFRAME.registerComponent('public-park', {
     init: function(){
 
         this.el.addEventListener('mouseleave', e => {
@@ -30,11 +33,29 @@ AFRAME.registerComponent('public-prak', {
         this.el.addEventListener('mouseenter', e => {
             this.el.setAttribute('animation',`property:scale; to:${buttonUpScale}; dur:${dur};`);
         })
-        
+
         this.el.addEventListener('click', e =>  {
             console.log(e);
         })
     }
 })
 
+
+
+AFRAME.registerComponent('private-park', {
+    init: function(){
+
+        this.el.addEventListener('mouseleave', e => {
+            this.el.setAttribute('animation',`property:scale; to:${buttonScale}; dur:${dur};`);
+        })
+
+        this.el.addEventListener('mouseenter', e => {
+            this.el.setAttribute('animation',`property:scale; to:${buttonUpScale}; dur:${dur};`);
+        })
+
+        this.el.addEventListener('click', e =>  {
+            console.log(e);
+        })
+    }
+})
 
