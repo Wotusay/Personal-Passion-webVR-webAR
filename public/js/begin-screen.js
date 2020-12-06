6b2562ce6d382c6e881ce277220226560334b6dd
wotusay <wout.salembier@hotmail.com>
2020-12-06 17:23:23 +0100

    added numpad

--- public/js/begin-screen.js
+++ public/js/begin-screen.js
@@ -1,6 +1,8 @@
 const buttonScale = "0.7 0.7 0.7";
 const buttonUpScale = "0.9 0.9 0.9";
 const dur = '500';
+const buttonColor = '#000000';
+const butttonRecolor = "#5348ea";
 
 AFRAME.registerComponent('start-event', {
     init: function() {
@@ -55,20 +57,128 @@ AFRAME.registerComponent('public-park', {
 
 AFRAME.registerComponent('private-park', {
     init: function(){
+        const publicButton = document.querySelector('.public');
+        const menu = document.querySelector('.menu');
+        const privateMenu = document.querySelector('.menu_private');
+        const privateButtonCreate = document.querySelector('.create');
+        const privateButtonJoin = document.querySelector('.join');
 
         this.el.addEventListener('mouseleave', e => {
-            this.el.classList.remove('clickable');
             this.el.setAttribute('animation',`property:scale; to:${buttonScale}; dur:${dur};`);
         })
 
         this.el.addEventListener('mouseenter', e => {
-            console.log(this.el)
             this.el.setAttribute('animation',`property:scale; to:${buttonUpScale}; dur:${dur};`);
         })
 
         this.el.addEventListener('click', e =>  {
+            this.el.classList.remove('clickable');
+            menu.setAttribute('visible','false'); 
+            publicButton.classList.remove('clickable');
 
+            privateMenu.setAttribute('visible', 'true');
+            privateButtonCreate.classList.add('clickable');
+            privateButtonJoin.classList.add('clickable');
         })
     }
 });
 
+AFRAME.registerComponent('create', {
+    init: function(){
+        const createMenu = document.querySelector('.create_menu');
+        const privateButtonJoin = document.querySelector('.join');
+        const privateMenu = document.querySelector('.menu_private');
+        const numpadButons = document.querySelectorAll('.numpad');
+
+        this.el.addEventListener('mouseleave', e => {
+            this.el.setAttribute('animation',`property:scale; to:${buttonScale}; dur:${dur};`);
+
+        })
+        this.el.addEventListener('mouseenter', e => {
+            this.el.setAttribute('animation',`property:scale; to:${buttonUpScale}; dur:${dur};`);
+ 
+        })
+        this.el.addEventListener('click', e => {
+            this.el.classList.remove('clickable');
+            privateButtonJoin.classList.remove('clickable');
+            privateMenu.setAttribute('visible','false');
+            createMenu.setAttribute('visible','true');
+            numpadButons.forEach(numpad => {
+                numpad.classList.add('clickable');
+            })
+        })
+    }
+});
+
+AFRAME.registerComponent('join', {
+    init: function(){
+        this.el.addEventListener('mouseleave', e => {
+            this.el.setAttribute('animation',`property:scale; to:${buttonScale}; dur:${dur};`);
+
+        })
+        this.el.addEventListener('mouseenter', e => {
+            this.el.setAttribute('animation',`property:scale; to:${buttonUpScale}; dur:${dur};`);
+ 
+        })
+        this.el.addEventListener('click', e => {
+            
+        })
+    }
+});
+
+AFRAME.registerComponent('join-menu', {
+    init: function(){
+        this.el.addEventListener('mouseleave', e => {
+            this.el.setAttribute('animation',`property:scale; to:${buttonScale}; dur:${dur};`);
+
+        })
+        this.el.addEventListener('mouseenter', e => {
+            this.el.setAttribute('animation',`property:scale; to:${buttonUpScale}; dur:${dur};`);
+ 
+        })
+        this.el.addEventListener('click', e => {
+            
+        })
+    }
+});
+
+AFRAME.registerComponent('numpad', {
+    init: function(){
+        this.el.addEventListener('mouseleave', e => {
+            this.el.setAttribute('animation',`property:color; to:${buttonColor}; dur:${dur};`);
+
+        })
+        this.el.addEventListener('mouseenter', e => {
+            this.el.setAttribute('animation',`property:color; to:${butttonRecolor}; dur:${dur};`);
+ 
+        })
+        this.el.addEventListener('click', e => {
+            
+        })
+    }
+});
+
+
+
+
+AFRAME.registerComponent('create-menu', {
+    init: function(){
+
+    }
+});
+
+
+
+AFRAME.registerComponent('back', {
+    init: function(){
+        this.el.addEventListener('mouseleave', e => {
+            this.el.setAttribute('animation',`property:scale; to:${buttonScale}; dur:${dur};`);
+        })
+        this.el.addEventListener('mouseenter', e => {
+            this.el.setAttribute('animation',`property:scale; to:${buttonUpScale}; dur:${dur};`); 
+        })
+        this.el.addEventListener('click', e => {
+            
+        })
+    }
+});
