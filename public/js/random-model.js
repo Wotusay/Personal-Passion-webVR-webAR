ecf4bb5e04647528216d433ff9e13cf59ef3a6c8
wotusay <wout.salembier@hotmail.com>
2020-12-10 19:32:57 +0100

    Adde an radoomizer to the models

--- public/js/random-model.js
+++ public/js/random-model.js
@@ -1,46 +1,72 @@
 AFRAME.registerComponent('random-model', {
     init: function() {
-        const modelHead = document.querySelector('.randomHead');
-        const modelBody = document.querySelector('.randomBody');
-
         let randomNumberGen = Math.floor(Math.random() * 6);
-        console.log(randomNumberGen);
+        const randomHead = 'randomHead';
+        const randomBody = 'randomBody';
+        console.log(this.el.className);
 
         if (randomNumberGen === 0 ) {
-            modelHead.setAttribute('src', '#player-head');
-            modelBody.setAttribute('src', '#player-body');
+            if (this.el.className === randomHead){
+                this.el.setAttribute('src', '#player-head');
+            };
 
-        }
+            if (this.el.className === randomBody) {
+                this.el.setAttribute('src', '#player-body');
+            };
+            
+        };
 
         if (randomNumberGen === 1 ) {
-            modelHead.setAttribute('src', '#player-head');
-            modelBody.setAttribute('src', '#player-body');
 
-        }
+            if (this.el.className === randomHead){
+                this.el.setAttribute('src', '#player-head-o');
+            };
+
+            if (this.el.className === randomBody) {
+                this.el.setAttribute('src', '#player-body-o');
+            };
+
+        };
 
         if (randomNumberGen === 2 ) {
-            modelHead.setAttribute('src', '#player-head');
-            modelBody.setAttribute('src', '#player-body');
+            if (this.el.className === randomHead){
+                this.el.setAttribute('src', '#player-head-p');
+            };
 
-        }
+            if (this.el.className === randomBody) {
+                this.el.setAttribute('src', '#player-body-p');
+            };
+        };
 
         if (randomNumberGen === 3 ) {
-            modelHead.setAttribute('src', '#player-head');
-            modelBody.setAttribute('src', '#player-body');
+            if (this.el.className === randomHead){
+                this.el.setAttribute('src', '#player-head-b');
+            };
 
-        }
+            if (this.el.className === randomBody) {
+                this.el.setAttribute('src', '#player-body-b');
+            };
+        };
 
         if (randomNumberGen === 4 ) {
-            modelHead.setAttribute('src', '#player-head');
-            modelBody.setAttribute('src', '#player-body');
+            if (this.el.className === randomHead){
+                this.el.setAttribute('src', '#player-head-r');
+            };
 
-        }
+            if (this.el.className === randomBody) {
+                this.el.setAttribute('src', '#player-body-r');
+            };
+        };
 
         if (randomNumberGen === 5 ) {
-            modelHead.setAttribute('src', '#player-head');
-            modelBody.setAttribute('src', '#player-body');
+            if (this.el.className === randomHead){
+                this.el.setAttribute('src', '#player-head-y');
+            };
 
-        }
+            if (this.el.className === randomBody) {
+                this.el.setAttribute('src', '#player-body-y');
+            };
+        };
     }
 
 });
