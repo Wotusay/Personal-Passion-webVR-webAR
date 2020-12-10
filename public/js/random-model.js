299e0171b8ce566cd48e96f4181495d4dba94f13
wotusay <wout.salembier@hotmail.com>
2020-12-10 18:01:38 +0100

    added the js for random body / heads

--- public/js/random-model.js
+++ public/js/random-model.js
@@ -1,11 +1,46 @@
 AFRAME.registerComponent('random-model', {
     init: function() {
-        let el = this.el;
-        console.log(el);
+        const modelHead = document.querySelector('.randomHead');
+        const modelBody = document.querySelector('.randomBody');
 
         let randomNumberGen = Math.floor(Math.random() * 6);
-
         console.log(randomNumberGen);
+
+        if (randomNumberGen === 0 ) {
+            modelHead.setAttribute('src', '#player-head');
+            modelBody.setAttribute('src', '#player-body');
+
+        }
+
+        if (randomNumberGen === 1 ) {
+            modelHead.setAttribute('src', '#player-head');
+            modelBody.setAttribute('src', '#player-body');
+
+        }
+
+        if (randomNumberGen === 2 ) {
+            modelHead.setAttribute('src', '#player-head');
+            modelBody.setAttribute('src', '#player-body');
+
+        }
+
+        if (randomNumberGen === 3 ) {
+            modelHead.setAttribute('src', '#player-head');
+            modelBody.setAttribute('src', '#player-body');
+
+        }
+
+        if (randomNumberGen === 4 ) {
+            modelHead.setAttribute('src', '#player-head');
+            modelBody.setAttribute('src', '#player-body');
+
+        }
+
+        if (randomNumberGen === 5 ) {
+            modelHead.setAttribute('src', '#player-head');
+            modelBody.setAttribute('src', '#player-body');
+
+        }
     }
 
 });
