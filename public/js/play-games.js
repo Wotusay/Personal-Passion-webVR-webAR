7a80f4af552192cf584675cd4c9de6aa76f50c07
wotusay <wout.salembier@hotmail.com>
2020-12-12 18:09:14 +0100

    Added changes + hoopes in progress'

--- public/js/play-games.js
+++ public/js/play-games.js
@@ -1,13 +1,21 @@
-AFRAME.registerComponent('basket-counter', {
+AFRAME.registerComponent('hooping', {
         init: function() {
+            let counter = 0;
+            const activityBoardBasket = document.querySelector('.progress__basket');
 
             this.el.addEventListener('collisions', (e) => {
-                
-        });
+                if (counter === 5) {
+                    activityBoardBasket.setAttribute('value', `#10b240`);
+                    activityBoardBasket.setAttribute('color', `Hoops made: ${counter}/5`)
+                    return;
+                }
+                console.log(e);
+                counter++;
+                activityBoardBasket.setAttribute('value', `Hoops made: ${counter}/5`);
+            });
         
     }
 });
-
 
 const spawnBox = () => {
     let box = document.createElement('a-box');
@@ -29,16 +37,20 @@ AFRAME.registerComponent('add-boxes', {
     init: function () {
         let counter = 2; 
         const addText = document.querySelector('.add__button');
-            this.el.addEventListener('collisions', (e) => {
-                if (counter === 7 ){
+        const activityBoardBoxes = document.querySelector('.progress__boxes');
+
+        this.el.addEventListener('collisions', (e) => {
+                if (counter === 8 ){
                     addText.setAttribute('value', 'Max boxes reached');
+                    activityBoardBoxes.setAttribute('color', `#10b240`);
+                    activityBoardBoxes.setAttribute('value', `Boxes placed: ${counter}/8`);
                     return; 
                 } else {
                     console.log(e);
                     counter++;
                     spawnBox();
                     console.log(counter);
-                    
+                    activityBoardBoxes.setAttribute('value', `Boxes placed: ${counter}/8`);
                 }
             });
         }
